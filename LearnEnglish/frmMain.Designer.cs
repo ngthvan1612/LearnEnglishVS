43c89b0486109102c3c2a63f55cd276faaf98b33
ngthvan1612 <41788391+ngthvan1612@users.noreply.github.com>
2019-03-07 19:57:29 +0700

    Initial commit

---
+++ LearnEnglish/frmMain.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace LearnEnglish
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listWords = new System.Windows.Forms.DataGridView();
+            this.tbCol_Eng = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbCol_Viet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.num = new System.Windows.Forms.NumericUpDown();
+            this.lbRest = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lbWord = new System.Windows.Forms.Label();
+            this.lbPoint = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listWords)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.Orange;
+            this.btnLoad.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoad.FlatAppearance.BorderSize = 0;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.ForeColor = System.Drawing.Color.White;
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(103, 32);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Orange;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(121, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(103, 32);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Orange;
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(230, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(103, 32);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Orange;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(339, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Edit";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Orange;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(448, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(103, 32);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listWords);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.groupBox1.Location = new System.Drawing.Point(12, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(539, 192);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Words";
+            // 
+            // listWords
+            // 
+            this.listWords.BackgroundColor = System.Drawing.Color.White;
+            this.listWords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listWords.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tbCol_Eng,
+            this.tbCol_Viet});
+            this.listWords.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listWords.GridColor = System.Drawing.Color.Black;
+            this.listWords.Location = new System.Drawing.Point(3, 16);
+            this.listWords.Name = "listWords";
+            this.listWords.Size = new System.Drawing.Size(533, 173);
+            this.listWords.TabIndex = 0;
+            this.listWords.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.listWords_CellContentClick);
+            this.listWords.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.listWords_CellValueChanged);
+            this.listWords.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.listWords_RowPostPaint);
+            // 
+            // tbCol_Eng
+            // 
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCol_Eng.DefaultCellStyle = dataGridViewCellStyle1;
+            this.tbCol_Eng.HeaderText = "Tiếng anh";
+            this.tbCol_Eng.Name = "tbCol_Eng";
+            this.tbCol_Eng.Width = 245;
+            // 
+            // tbCol_Viet
+            // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Times New Roman", 11.25F);
+            this.tbCol_Viet.DefaultCellStyle = dataGridViewCellStyle2;
+            this.tbCol_Viet.HeaderText = "Tiếng Việt";
+            this.tbCol_Viet.Name = "tbCol_Viet";
+            this.tbCol_Viet.Width = 245;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.num);
+            this.groupBox2.Controls.Add(this.lbRest);
+            this.groupBox2.Controls.Add(this.tbInput);
+            this.groupBox2.Controls.Add(this.lbWord);
+            this.groupBox2.Controls.Add(this.lbPoint);
+            this.groupBox2.Location = new System.Drawing.Point(12, 248);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(539, 110);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Learn";
+            // 
+            // num
+            // 
+            this.num.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num.Location = new System.Drawing.Point(93, 46);
+            this.num.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(120, 26);
+            this.num.TabIndex = 4;
+            this.num.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lbRest
+            // 
+            this.lbRest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbRest.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRest.Location = new System.Drawing.Point(6, 46);
+            this.lbRest.Name = "lbRest";
+            this.lbRest.Size = new System.Drawing.Size(81, 23);
+            this.lbRest.TabIndex = 3;
+            this.lbRest.Text = "Repeat";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbInput.Location = new System.Drawing.Point(6, 78);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(527, 32);
+            this.tbInput.TabIndex = 2;
+            this.tbInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbInput_KeyDown);
+            // 
+            // lbWord
+            // 
+            this.lbWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbWord.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbWord.Location = new System.Drawing.Point(93, 16);
+            this.lbWord.Name = "lbWord";
+            this.lbWord.Size = new System.Drawing.Size(440, 23);
+            this.lbWord.TabIndex = 1;
+            this.lbWord.Text = "Word";
+            this.lbWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbPoint
+            // 
+            this.lbPoint.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbPoint.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPoint.Location = new System.Drawing.Point(6, 16);
+            this.lbPoint.Name = "lbPoint";
+            this.lbPoint.Size = new System.Drawing.Size(81, 23);
+            this.lbPoint.TabIndex = 0;
+            this.lbPoint.Text = "Point";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 370);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Learn English";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listWords)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView listWords;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tbCol_Eng;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tbCol_Viet;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbWord;
+        private System.Windows.Forms.Label lbPoint;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label lbRest;
+        private System.Windows.Forms.NumericUpDown num;
+
+    }
+}
+
---
+++ LearnEnglish/frmMain.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using System.IO;
+
+namespace LearnEnglish
+{
+    public partial class frmMain : Form
+    {
+        private StreamWords sw = null;
+        private string path = null;
+        private Word Current;
+        private List<Word> queue;
+        private int PointST = 0;
+        private const string Filter = "Learn English|*.learn";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            sw = new StreamWords();
+            Current.Eng = "\0";
+            queue = new List<Word>();
+        }
+
+        private void listWords_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void listWords_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            var gird = sender as DataGridView;
+            var rowIndex = (e.RowIndex + 1).ToString();
+            var centerFormat = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+            var headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, gird.RowHeadersWidth, e.RowBounds.Height);
+            e.Graphics.DrawString(rowIndex, this.Font, SystemBrushes.ControlText, headerBounds, centerFormat);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var current = listWords.CurrentRow;
+            if (current != null && current.Index != listWords.NewRowIndex)
+                listWords.Rows.Remove(current);
+        }
+
+        private void FillToList()
+        {
+            listWords.Rows.Clear();
+            foreach (Word w in sw.listWords)
+                listWords.Rows.Add(w.Eng, w.Viet);
+        }
+
+        private void OpenFile()
+        {
+            if (sw != null)
+            {
+                OpenFileDialog o = new OpenFileDialog()
+                {
+                    Filter = Filter,
+                    Multiselect = false
+                };
+                if (o.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    sw.LoadData(o.FileName);
+                    FillToList();
+                }
+            }
+        }
+
+        private void SaveFile()
+        {
+            if (sw != null)
+            {
+                SaveFileDialog s = new SaveFileDialog()
+                {
+                    Filter = Filter,
+                    FileName = sw.Path
+                };
+                if (s.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    sw.listWords.Clear();
+                    for (int i = 0; i < listWords.Rows.Count; ++i)
+                    {
+                        object eng = listWords.Rows[i].Cells[0].Value;
+                        object viet = listWords.Rows[i].Cells[1].Value;
+                        if (eng == null || viet == null) continue;
+                        sw.listWords.Add(new Word(eng.ToString(), viet.ToString()));
+                    }
+                    sw.WriteData(s.FileName);
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (sw == null) return;
+            if (sw.Saved == false)
+            {
+                switch (MessageBox.Show("Bạn có muốn lưu lại không?", "Warn", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning))
+                {
+                    case System.Windows.Forms.DialogResult.Yes:
+                        SaveFile();
+                        if (sw.Saved == false) return;
+                        break;
+                    case System.Windows.Forms.DialogResult.No:
+                        OpenFile();
+                        return;
+                    case System.Windows.Forms.DialogResult.Cancel:
+                        return;
+                }
+            }
+            if (sw.Saved == true) OpenFile();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (sw == null) return;
+            SaveFile();
+        }
+
+        private void listWords_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (sw == null) return;
+            sw.Saved = false;
+        }
+
+        private Word GetNextWord()
+        {
+            if (queue.Count == 0)
+            {
+
+                return Current = new Word("\0", "");
+            }
+            Current = queue[0];
+            queue.RemoveAt(0);
+            lbWord.Text = Current.Viet;
+            return Current;
+        }
+
+        private void tbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                if (Current.Eng == "\0")
+                {
+                    MessageBox.Show("Hết từ vựng!");
+                    return;
+                }
+                if (tbInput.Text.ToUpper() == Current.Eng.ToUpper())
+                {
+                    lbPoint.Text = (++PointST).ToString();
+                    Current = GetNextWord();
+                }
+                else
+                {
+                    MessageBox.Show(Current.Eng);
+                    lbPoint.Text = (--PointST).ToString();
+                }
+                tbInput.Text = "";
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            queue.Clear();
+            {
+                lbPoint.Text = (PointST = 0).ToString();
+                ///New
+                Random r = new Random();
+                for (int i = 0; i < num.Value; ++i)
+                {
+                    foreach (Word w in sw.listWords)
+                    {
+                        Word tmp = w;
+                        tmp.id = r.Next();
+                        queue.Add(tmp);
+                    }
+                }
+                for (int i = 0; i < queue.Count - 1; ++i)
+                    for (int j = i + 1; j < queue.Count; ++j)
+                        if (queue[i].id > queue[j].id) {
+                            Word tmp = queue[i];
+                            queue[i] = queue[j];
+                            queue[j] = tmp;
+                        }
+                Current = GetNextWord();
+            }
+            tbInput.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("No information!");
+        }
+    }
+
+    public class StreamWords
+    {
+        public List<Word> listWords;
+        public bool Saved;
+        public string Path;
+
+        public StreamWords()
+        {
+            listWords = new List<Word>();
+            Saved = true;
+            Path = "";
+        }
+
+        public void ResetData()
+        {
+            listWords = new List<Word>();
+            Saved = true;
+            Path = "";
+        }
+
+        public void LoadData(string path)
+        {
+            Path = path;
+            Saved = true;
+            ResetData();
+            BinaryReader br = new BinaryReader(new FileStream(path, FileMode.OpenOrCreate));
+            int n = br.ReadInt32();
+            for (int i = 0; i < n; ++i)
+            {
+                string en, vi;
+                en = br.ReadString();
+                vi = br.ReadString();
+                listWords.Add(new Word(en, vi));
+            }
+            br.Close();
+        }
+
+        public void Add(string en, string vi)
+        {
+            Saved = false;
+            listWords.Add(new Word(en, vi));
+        }
+
+        public void WriteData(string path)
+        {
+            Path = path;
+            Saved = true;
+            BinaryWriter bw = new BinaryWriter(new FileStream(path, FileMode.Create));
+            bw.Write(listWords.Count);
+            foreach (Word w in listWords)
+            {
+                bw.Write(w.Eng);
+                bw.Write(w.Viet);
+            }
+            bw.Close();
+        }
+    }
+
+    public struct Word
+    {
+        public string Eng;
+        public string Viet;
+        public int id;
+        public Word(string _eng, string _viet)
+        {
+            id = 0;
+            Eng = _eng;
+            Viet = _viet;
+        }
+    }
+}
